660f5e53bb65dc2ff1fb62292d923dff9df4da85
WoeunJung <leena09123@naver.com>
2023-06-29 11:20:18 +0900

    feat: navigate of categoryScreen

--- app/src/main/java/com/example/juda_kotlin/app/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/juda_kotlin/app/presentation/navigation/NavGraph.kt
@@ -3,9 +3,7 @@ package com.example.juda_kotlin.app.presentation.navigation
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.example.juda_kotlin.app.presentation.view.DetailMentorScreen
-import com.example.juda_kotlin.app.presentation.view.JudaMainScreen
-import com.example.juda_kotlin.app.presentation.view.SplashScreen
+import com.example.juda_kotlin.app.presentation.view.*
 
 fun NavGraphBuilder.judaGraph(
     navController: NavController
@@ -13,4 +11,7 @@ fun NavGraphBuilder.judaGraph(
     composable(route = Screen.SplashScreen.route) { SplashScreen(navController) }
     composable(route = Screen.JudaMainScreen.route) { JudaMainScreen(navController) }
     composable(route = Screen.DetailMentorScreen.route) { DetailMentorScreen(navController)}
+    composable(route = Screen.SignInScreen.route) { SignInScreen(navController) }
+    composable(route = Screen.BigCategoryScreen.route) { BigCategoryScreen(navController)}
+    composable(route = Screen.SmallCategoryScreen.route) { SmallCategoryScreen(navController) }
 }
