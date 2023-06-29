778135e4444531142f970b4b8f4312b05294590e
WoeunJung <leena09123@naver.com>
2023-06-29 13:58:28 +0900

    feat: kakao Login

--- app/src/main/java/com/example/juda_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/juda_kotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.juda_kotlin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +17,11 @@ import com.example.juda_kotlin.app.presentation.navigation.Screen
 import com.example.juda_kotlin.app.presentation.navigation.judaGraph
 import com.example.juda_kotlin.app.presentation.view.JudaMainScreen
 import com.example.juda_kotlin.ui.theme.Juda_KotlinTheme
+import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/juda_kotlin/app/presentation/view/SignInScreen.kt
+++ app/src/main/java/com/example/juda_kotlin/app/presentation/view/SignInScreen.kt
@@ -3,17 +3,24 @@ package com.example.juda_kotlin.app.presentation.view
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.juda_kotlin.app.presentation.navigation.Screen
+import com.example.juda_kotlin.app.presentation.viewmodel.loginViewModel
 
 @Composable
 fun SignInScreen(
-    navController: NavController
+    navController: NavController,
+    loginViewModel: loginViewModel = hiltViewModel()
 ){
-    Button(onClick = { navController.navigate(Screen.BigCategoryScreen.route)}) {
+    val context = LocalContext.current
+    Button(onClick = {
+        navController.navigate(Screen.BigCategoryScreen.route)
+        loginViewModel.kakaoLogin(context)
+    }) {
         Text(
-           text = "카카오톡으로 로그인하기"
+           text = "카카오톡으로 로그인하기",
         )
     }
-
 }
